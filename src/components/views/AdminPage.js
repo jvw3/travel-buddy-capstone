0ca8dae55cd9edd0d91dd5cf8c155d736631381e
Jonathan Woodard <jvw.226@gmail.com>
2022-09-13 14:51:00 -0500

    Added home pages for users that are travelers and for the admin user

---
+++ src/components/views/AdminPage.js
@@ -0,0 +1,7 @@
+
+
+export const AdminHomePageView = () => {
+    return (
+        <h1>Currently Logged in as admin</h1>
+    )
+}
---
+++ src/components/views/TravelerHomePage.js
@@ -0,0 +1,8 @@
+import "./HomePage.css"
+
+
+export const HomePageView = () => {
+    return (
+        <h1>Travel Buddy</h1>
+    )
+}

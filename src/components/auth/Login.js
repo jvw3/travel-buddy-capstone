fd9010a112de65e5ef9cf062f40b53e4266c67c6
Jonathan Woodard <jvw.226@gmail.com>
2022-09-13 14:40:28 -0500

    fixed logic for login and register components

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -10,7 +10,7 @@ export const Login = () => {
     const handleLogin = (e) => {
         e.preventDefault()
 
-        return fetch(`http://localhost:8080/users?email=${email}`)
+        return fetch(`http://localhost:8088/users?email=${email}`)
             .then(res => res.json())
             .then(foundUsers => {
                 if (foundUsers.length === 1) {

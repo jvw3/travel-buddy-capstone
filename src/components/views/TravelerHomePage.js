0ff2ba88523a9a59726aef654756aa8b263b6a51
Jonathan Woodard <jvw.226@gmail.com>
2022-09-22 12:19:27 -0500

    updated Hompage CSS, fixed error that was causing front page to not update information of user, updated NavBar CSS

--- src/components/views/TravelerHomePage.js
+++ src/components/views/TravelerHomePage.js
@@ -4,11 +4,14 @@ import "./HomePage.css";
 import { UserItineraries } from "../itineraries/MyTrips";
 import { UpcomingTrip } from "./HomePageUpcomingTrips";
 
-const localAppUser = localStorage.getItem("travelbuddy_user");
-const appUserObject = JSON.parse(localAppUser);
+
 
 export const HomePageView = () => {
   const [userItineraries, setUserItineraries] = useState([]);
+  const [currentUser, setCurrentUser] = useState({});
+
+  const localAppUser = localStorage.getItem("travelbuddy_user");
+  const appUserObject = JSON.parse(localAppUser);
 
   useEffect(() => {
     fetch(
@@ -20,22 +23,35 @@ export const HomePageView = () => {
       });
   }, []);
 
+  useEffect(() => {
+    fetch(`http://localhost:8099/users/${appUserObject?.id}`)
+      .then((res) => res.json())
+      .then((currentUser) => {
+        setCurrentUser(currentUser);
+      });
+  }, []);
+
   //   const displayedUpcomingTrips = userItineraries.slice(0, 3);
 
   return (
     <>
       <div className="topsection">
         <div class="topsectionoverlay">
-          <h1 className="homepageheader">Travel Buddy</h1>
+          <div class="welcomemessage">
+            <h1>Welcome Back, {currentUser?.name}</h1>
+          </div>
+          <div className="getstartedtext">
+          <h2 className="homepageheader">Travel Buddy</h2>
           <Link className="getStarted" to="/createnewtrip">
             Get started!
           </Link>
+          </div>
         </div>
       </div>
       <div className="bottomsection">
         <h2>Upcoming Trips</h2>
         <section className="upcomingtripcontainer">
-          {userItineraries.map(trip => (
+          {userItineraries.map((trip) => (
             <UpcomingTrip
               key={`itinerary--${trip?.id}`}
               tripObject={trip}

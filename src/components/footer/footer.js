7cfe0191ae46d73491e2a20ac7a5cf921c90d89d
Jonathan Woodard <jvw.226@gmail.com>
2022-09-28 13:22:40 -0500

    added footer to website - still need to adjust positioning

---
+++ src/components/footer/footer.js
@@ -0,0 +1,15 @@
+import { Text } from "@mantine/core"
+import "./footer.css";
+
+export const Footer = () => {
+
+
+
+    return <>
+    <footer className="footersection">
+        <Text color="white" > About</Text>
+        <Text color="white" > LinkedIn</Text>
+        <Text color="white" >Github</Text>
+    </footer>
+    </>
+}
--- src/components/travelbuddy.js
+++ src/components/travelbuddy.js
@@ -4,7 +4,7 @@ import { ApplicationViews } from "./views/ApplicationViews"
 import { NavBar } from "./navbar/NavBar"
 import { Login } from "./auth/Login"
 import { Register } from "./auth/Register"
-
+import { Footer} from "./footer/footer"
 //This component is responsible for rendering the login information, and if the user is a travelbuddy user (traveler or admin), they will be directed into the application with the appropriate view. If they are not a travelbuddy user...
 export const TravelBuddy = () => {
 	return <Routes>
@@ -17,6 +17,7 @@ export const TravelBuddy = () => {
 				<>
 					<NavBar />
 					<ApplicationViews />
+					<Footer />
 				</>
 			</Authorized>
 

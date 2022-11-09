068a9bb65f67735e606904964d6fad3ed483f7be
Jonathan Woodard <jvw.226@gmail.com>
2022-11-09 10:21:49 -0600

    add comments and add useFetch function to module from api manager

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,65 +1,20 @@
 import { useEffect, useState } from "react";
 import { Card, Text, Button, Title, Center } from "@mantine/core";
-import { useNavigate } from "react-router-dom";
-
+import { json, useNavigate } from "react-router-dom";
+import { useFetch } from "../api/APImanager";
 //This component is responsible for rendering the full view of a users profile.
 export const Profile = ({ searchTermState }) => {
   const [user, setUser] = useState({});
-  const [userItineraries, setUserItineraries] = useState([]);
-  const [itineraryActivities, setItineraryActivities] = useState([]);
-  const [filteredActivities, setFiltered] = useState([]);
 
   const navigate = useNavigate();
 
   const localAppUser = localStorage.getItem("travelbuddy_user");
   const appUserObject = JSON.parse(localAppUser);
 
-  //useEffect hook responsible for fetching the current user data and storing it in user state Variable.
-  useEffect(() => {
-    fetch(`http://localhost:8099/users/${appUserObject.id}`)
-      .then((res) => res.json())
-      .then((currentUserInfo) => {
-        setUser(currentUserInfo);
-      });
-  }, []);
+//useEffect hook responsible for fetching the current user data and storing it in user state Variable.
 
-// useEffect hook responsible for fetching the full array of itinerary activities.
-  useEffect(() => {
-    fetch(`http://localhost:8099/itineraryActivities`)
-      .then((res) => res.json())
-      .then((activities) => {
-        setItineraryActivities(activities);
-      });
-  }, []);
+useFetch(`http://localhost:8099/users/${appUserObject.id}`, setUser)
 
-// useEffect hook responsible for fetching all of the userItineraries for the current loggied in user.
-  useEffect(() => {
-    fetch(`http://localhost:8099/userItineraries?_expand=itinerary&userId=${appUserObject.userId}`)
-      .then((res) => res.json())
-      .then((userItinerariesArray) => {
-        setUserItineraries(userItinerariesArray);
-      });
-  }, []);
-
-
-
-  useEffect(() => {
-    const userActivities = [];
-    userItineraries.forEach((itinerary) => {
-      itineraryActivities.forEach((activity) => {
-        if (activity.itineraryId === itinerary.itineraryId) {
-          userActivities.push(activity);
-        }
-      });
-    });
-
-    const searchedActivities = userActivities.filter((activity) => {
-      return activity.description
-        .toLowerCase()
-        .startsWith(searchTermState.toLowerCase());
-    });
-    setFiltered(searchedActivities);
-  }, [searchTermState]);
 
   return (
     <>
@@ -89,6 +44,9 @@ export const Profile = ({ searchTermState }) => {
             <Text>Name: {user.fullName}</Text>
             <Text>Email: {user.email}</Text>
             <Text>Hometown: {user.hometown} </Text>
+            <Text>Total Trips Completed</Text>
+            <Text>Total Activities Completed</Text>
+            <Text>Total Reviews Shared</Text>
           </div>
         </Card>
       </div>
